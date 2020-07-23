4f77928d1086a728714aa71a6dc67b43d819f413
habonimana aime parfait <aphabona@gmail.com>
2020-07-23 21:35:28 +0200

    commit

---
+++ Devoir/Automobile.php
@@ -0,0 +1,39 @@
+<?php
+//declaration d'une classe mère' Heritage'
+class Automobile
+{
+private $categorie;
+private $plaque;
+private $marque;
+private $couleur;
+
+
+//declaration d'un constructeur
+
+public function __construct($categorie,$plaque,$marque,$couleur){
+
+    $this->categorie=$categorie;
+    $this->plaque=$plaque;
+    $this->marque=$marque;
+    $this->couleur=$couleur;
+    }
+    //declararion d'un destructeur
+public function destruct(){
+      #code..   
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
